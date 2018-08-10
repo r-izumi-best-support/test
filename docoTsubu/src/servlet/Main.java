fbc5743453e259317fd1320b9d9930dff4060094
BSSPC083\BSS_Sapporo_K02_K <r-izumi@best-support.com>
2018-08-10 13:43:48 +0900

    no message

---
+++ docoTsubu/src/servlet/Main.java
@@ -0,0 +1,65 @@
+﻿package servlet;
+
+import java.io.*;
+import java.util.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+import model.*;
+
+/**
+ * Servlet implementation class Main
+ */
+@WebServlet("/Main")
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		GetMutterListLogic getMutterListLogic = new GetMutterListLogic();
+		List<Mutter> mutterList = getMutterListLogic.execute();
+		request.setAttribute("mutterList", mutterList);
+
+		HttpSession session = request.getSession();
+		User loginUser = (User) session.getAttribute("loginUser");
+
+		if (loginUser == null) {
+			response.sendRedirect("/docoTsubu/");
+		} else {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String text = request.getParameter("text");
+
+		if (text != null && text.length() != 0) {
+
+			HttpSession session = request.getSession();
+			User loginUser = (User) session.getAttribute("loginUser");
+
+			Mutter mutter = new Mutter(loginUser.getName(), text);
+			PostMutterLogic postMutterLogic = new PostMutterLogic();
+			postMutterLogic.execute(mutter);
+
+			GetMutterListLogic getMutterListLogic = new GetMutterListLogic();
+			List<Mutter> mutterList = getMutterListLogic.execute();
+			request.setAttribute("mutterList", mutterList);
+
+		} else {
+			request.setAttribute("errorMsg", "つぶやきが入力されていません");
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/main.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
